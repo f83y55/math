c6869460cddc39cd1b706d12821bf90108fe7db8
f83y55 <f83y55@gmail.com>
2023-11-12 22:37:43 +0100

    commit

--- s.js
+++ s.js
@@ -1,10 +1,10 @@
 // site root / import_modules / title
+
+
 const root_path = document.head.getElementsByTagName("script")[0].getAttribute("src").replace('s.js', '');
 console.log("root_path : "+root_path);
 const import_module = document.head.getElementsByTagName("script")[0].hasAttribute("data-import") ? document.head.getElementsByTagName("script")[0].getAttribute("data-import").split(" ") : [];
 console.log("import_module : " + import_module);
-const page_title = document.head.getElementsByTagName("title")[0].text;
-console.log("page_title : " + page_title);
 
 // Modules
 
@@ -33,12 +33,9 @@ const modules_head = {
 
 function inject_cssjs (hashtable, recipient, import_module) {
 for (var key in hashtable){
-	console.log("key : " + key);
 	for (var item of hashtable[key]){
 		console.log("path : " + item["path"] + "| default : " + item["default"]);
-			console.log(((item["path"].endsWith(".css"))&&(item["default"]||(import_module.includes(key)))));
 		if ((item["path"].endsWith(".css"))&&(item["default"]||(import_module.includes(key)))) {
-			console.log("link : " + item["path"]);
 			const element = document.createElement("link");
 			element.setAttribute("href", root_path + item["path"]);
 			element.setAttribute("rel", "stylesheet");
@@ -49,7 +46,6 @@ for (var key in hashtable){
 			recipient.appendChild(element);
 		}
 		if ((item["path"].endsWith("js"))&&((item["default"])||(import_module.includes(key)))) {
-			console.log("script : " + item["path"]);
 			const element = document.createElement("script");
 			for (attr in item["attr"]) { 
 				element.setAttribute(attr, item["attr"][attr])
@@ -84,57 +80,65 @@ function body () {
     inject_cssjs (modules_head, document.head, import_module);
 	apply_rootpaths();
 
+// head : title, metas
+const before_title = "Mathématiques - ";
+const page_title = before_title + document.getElementsByTagName("h1")[0].textContent.trim();
+const title = document.head.getElementsByTagName("title")[0];
+title.text = page_title;
+Array.from(document.head.getElementsByTagName("meta")).forEach(function(el){
+	return 0; // afaire
+});
+
+// body : header and footer
 const header = document.createElement("header");
 document.body.prepend(header);
 const footer = document.createElement("footer");
 document.body.append(footer);
 
-
-
-
-
 header.innerHTML += `
-<nav class=\"navbar\">
-<input type=\"checkbox\" id=\"navbar-menu-switch\" style=\"display:none;\"/>
-<label for=\"navbar-menu-switch\" class=\"navbar-icon navbar-logo\"></label>
-<div class=\"navbar-collapse\">                 <div class=\"navbar-group\">                     <div class=\"navbar-item\">
-				<h2 class=\"nocounter navbar-text\">
-				Mathématiques
-				</h2>
-				<div class=\"navbar-text\">
-				Cours</div>
-				</div>
-				</div>
-				<div class=\"navbar-group\">
-				<a class=\"navbar-item\" id=\"navbar-index\" href=\"${root_path}/index.html\">
-				<img class=\"navbar-icon\" src=\"${root_path}layouts/nav/assets/home.svg\"/>
-				<div class=\"navbar-text\">home</div>
-				</a>
-				<a class=\"navbar-item\" href=\"https://www.geogebra.org/classic\" target=\"_blank\">
-				<img class=\"navbar-icon\" src=\"${root_path}layouts/nav/assets/geogebra.svg\"/>
-				<div class=\"navbar-text\">GeoGebra</div>
-				</a>
-				<a class=\"navbar-item\" href=\"./assets/print.pdf\" download=\"${page_title}.pdf\">
-				<img class=\"navbar-icon\" src=\"${root_path}layouts/nav/assets/pdf.svg\">
-				<div class=\"navbar-text\">pdf</div>
-				</a>
-				<a class=\"navbar-item\" href=\"#\" onclick=\"window.print();return false;\">
-				<img class=\"navbar-icon\" src=\"${root_path}layouts/nav/assets/print.svg\"/>
-				<div class=\"navbar-text\">print</div>
-				</a>
-				</div>
-				<div class=\"navbar-group\">
-				<a class=\"navbar-item\" href=\"${root_path}pages/links/links.html\">
-				<img class=\"navbar-icon\" src=\"${root_path}layouts/nav/assets/links.svg\"/>
-				<div class=\"navbar-text\">links</div>
-				</a>
-				<a class=\"navbar-item\" href=\"https://github.com/f83y55/math/archive/refs/heads/master.zip\">
-				<img class=\"navbar-icon\" src=\"${root_path}layouts/nav/assets/download.svg\"/>
-				<div class=\"navbar-text\">download</div>
-				</a>
-				</div>
-				</div>
-				</nav>
+<nav class="navbar">
+<input type="checkbox" id="navbar-menu-switch" style="display:none;"/>
+<label for="navbar-menu-switch" class="navbar-icon navbar-logo"></label>
+	<div class="navbar-collapse">
+		<div class="navbar-group">                    
+			<div class="navbar-item">
+			<div class="navbar-text" style="font-size:larger;font-weight:bold;">
+			Mathématiques
+			</div>
+			<div class="navbar-text">
+			Cours</div>
+			</div>
+			</div>
+			<div class="navbar-group">
+			<a class="navbar-item" id="navbar-index" href="${root_path}/index.html">
+			<img class="navbar-icon" src="${root_path}layouts/nav/assets/home.svg"/>
+			<div class="navbar-text">home</div>
+			</a>
+			<a class="navbar-item" href="https://www.geogebra.org/classic" target="_blank">
+			<img class="navbar-icon" src="${root_path}layouts/nav/assets/geogebra.svg"/>
+			<div class="navbar-text">GeoGebra</div>
+			</a>
+			<a class="navbar-item" href="./assets/print.pdf" download="${page_title}.pdf">
+			<img class="navbar-icon" src="${root_path}layouts/nav/assets/pdf.svg"/>
+			<div class="navbar-text">pdf</div>
+			</a>
+			<a class="navbar-item" href="#" onclick="window.print();return false;">
+			<img class="navbar-icon" src="${root_path}layouts/nav/assets/print.svg"/>
+			<div class="navbar-text">print</div>
+			</a>
+			</div>
+			<div class="navbar-group">
+			<a class="navbar-item" href="${root_path}pages/links/links.html">
+			<img class="navbar-icon" src="${root_path}layouts/nav/assets/links.svg"/>
+			<div class="navbar-text">links</div>
+			</a>
+			<a class="navbar-item" href="https://github.com/f83y55/math/archive/refs/heads/master.zip">
+			<img class="navbar-icon" src="${root_path}layouts/nav/assets/download.svg"/>
+			<div class="navbar-text">download</div>
+			</a>
+		</div>
+	</div>
+</nav>
 				`
 };
 
